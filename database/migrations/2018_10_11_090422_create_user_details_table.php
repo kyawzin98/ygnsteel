029037faceff1c8b7d5be6f18ebc9f34839359df
bkjonathan <admin@sinphyukyun.com>
2018-10-11 15:41:33 +0630

    User Details

---
+++ database/migrations/2018_10_11_090422_create_user_details_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_details', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->text('address1')->nullable();
+            $table->text('address2')->nullable();
+            $table->string('township')->nullable();
+            $table->string('phone');
+            $table->string('nrc_no');
+            $table->string('business_type');
+            $table->string('working_bank')->nullable();
+            $table->string('bank_account_no')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_details');
+    }
+}
