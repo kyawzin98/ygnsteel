2a3c40a9bbf654e97eb7df0031de6b3fc625b737
bkjonathan <admin@sinphyukyun.com>
2018-10-14 11:12:59 +0630

    Register page update

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -6,22 +6,31 @@
          style="background-image: url('{{asset('t1/assets/app/media/img//bg/bg-7.jpg')}}');">
         <div class="m-grid__item m-grid__item--fluid	m-login__wrapper">
             <div class="m-login__container">
-                <div class="m-login__logo">
-                    <a href="#">
-                        <img src="{{asset('t1/assets/demo/demo8/media/img/logo/logo.png')}}" class="img-fluid" style="width: 150px;">
-                    </a>
-                </div>
+                {{--<div class="m-login__logo">--}}
+                    {{--<a href="#">--}}
+                        {{--<img src="{{asset('t1/assets/demo/demo8/media/img/logo/logo.png')}}" class="img-fluid" style="width: 150px;">--}}
+                    {{--</a>--}}
+                {{--</div>--}}
                 <div class="m-login__signin">
                     <div class="m-login__head">
                         <h2 class="j-text-1 text-center">
                             Yangon Steel Family
                         </h2>
-                        <h3 class="j-text-1 text-center mt-3">
-                            Please Sign in
-                        </h3>
+                        <h4 class="j-text-1 text-center mt-3">
+                            Sign up here..
+                        </h4>
                     </div>
-                    <form class="m-login__form m-form"  method="POST" action="{{ route('login') }}">
+                    <form class="m-login__form m-form"  method="POST" action="{{ route('register') }}">
                         {{ csrf_field() }}
+                        <div class="form-group m-form__group{{ $errors->has('name') ? ' has-danger' : '' }}">
+                            <input class="form-control m-input" value="{{ old('name') }}"  type="text" placeholder="Username" name="name" autocomplete="off">
+                            @if ($errors->has('name'))
+                                <div class="form-control-feedback">
+                                    <strong>{{ $errors->first('name') }}</strong>
+                                </div>
+                            @endif
+                        </div>
+
                         <div class="form-group m-form__group{{ $errors->has('email') ? ' has-danger' : '' }}">
                             <input class="form-control m-input" value="{{ old('email') }}"  type="text" placeholder="Email" name="email" autocomplete="off">
                             @if ($errors->has('email'))
@@ -38,23 +47,19 @@
                                 </div>
                             @endif
                         </div>
-                        <div class="row m-login__form-sub">
-                            <div class="col m--align-left m-login__form-left">
-                                <label class="m-checkbox  m-checkbox--light">
-                                    <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}>
-                                    Remember me
-                                    <span></span>
-                                </label>
-                            </div>
-                            <div class="col m--align-right m-login__form-right">
-                                <a href="{{ route('password.request') }}" id="m_login_forget_password" class="m-link">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
+
+                        <div class="form-group m-form__group{{ $errors->has('password') ? ' has-danger' : '' }}">
+                            <input class="form-control m-input m-login__form-input--last" value="{{ old('password_confirmation') }}" type="password" placeholder="Confirm Password" name="password_confirmation">
+                            @if ($errors->has('password'))
+                                <div class="form-control-feedback">
+                                    <strong>{{ $errors->first('password') }}</strong>
+                                </div>
+                            @endif
                         </div>
+
                         <div class="m-login__form-action">
                             <button id="m_login_signin_submit" class="btn m-btn--pill m-btn--air btn-outline-info btn-lg m-login__btn">
-                                Sign In
+                                Register
                             </button>
                         </div>
                     </form>
